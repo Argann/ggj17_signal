a5e254308573643f0bc1f18525ff73ed11b61a53
Argann Bonneau <argann.b@hotmail.fr>
2017-01-21 12:28:54 +0100

    Ajout du système de transition de caméra par waypoints

---
+++ Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransition : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private List<GameObject> waypoints;
+
+    private int cursor;
+
+	// Use this for initialization
+	void Start () {
+        cursor = 0;
+	}
+
+    public void Next() {
+        if (cursor < waypoints.Count) {
+            cursor++;
+            StartCoroutine(moveToNextPoint());
+        }
+    }
+
+    IEnumerator moveToNextPoint() {
+        while (transform.position != waypoints[cursor].transform.position) {
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[cursor].transform.position, Time.deltaTime * speed);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+}
